b500f9a52e9c8eae4567e8117b665285789d1c78
ThaoVTI <thao.duongvan@vti.com.vn>
2026-02-26 16:12:46 +0700

    fit_transform output data

--- VN30F1M/load/prepare_data_tf_5m.py
+++ VN30F1M/load/prepare_data_tf_5m.py
@@ -2,27 +2,58 @@ import warnings
 warnings.filterwarnings('ignore')
 import os
 import pandas as pd
+import argparse
 from VN30F1M.transform import OHLCV_DIR, DATA_READY_DIR
 from VN30F1M.transform.func_validating import validate_ohlcv_dataset
 from VN30F1M.transform.func_cleaning import clean_ohlcv
 from VN30F1M.transform.func_features import feature_engineering
-from VN30F1M.transform.func_preprocessing import preprocessing_data
+from VN30F1M.transform.func_preprocessing import preprocessing_data, numeric_data
 from VN30F1M.transform.func_labeling import labeling_data
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Prepare OHLCV 5m data")
+
+    parser.add_argument(
+        "--limit",
+        type=int,
+        default=2000,
+        help="Number of last rows to load"
+    )
+
+    parser.add_argument(
+        "--to_number",
+        action="store_true",
+        help="Convert all object columns to numeric"
+    )
+
+    args = parser.parse_args()
+
     ohlcv_file = str(OHLCV_DIR) + '/VN30F1M_5m.csv'
     csv_ready_file = str(DATA_READY_DIR) + '/VN30F1M_5m_ready.csv'
-    is_file = os.path.isfile(ohlcv_file)
-    if is_file:
-        ohlcv_data = pd.read_csv(ohlcv_file, index_col='Date', parse_dates=True).tail(2000)
+    csv_ready_file_num = str(DATA_READY_DIR) + '/VN30F1M_5m_numeric_ready.csv'
+
+    if os.path.isfile(ohlcv_file):
+
+        ohlcv_data = pd.read_csv(
+            ohlcv_file,
+            index_col='Date',
+            parse_dates=True
+        ).tail(args.limit)
+
         is_validate, _ = validate_ohlcv_dataset(ohlcv_data)
+
         if is_validate:
             clean_data = clean_ohlcv(ohlcv_data)
+
             if len(clean_data) > 0:
                 featured_data = feature_engineering(clean_data)
                 labeled_data = labeling_data(featured_data)
                 preprocessed_data = preprocessing_data(labeled_data)
                 preprocessed_data.to_csv(csv_ready_file, index=True)
+                # Optional numeric conversion
+                if args.to_number:
+                    num_data = numeric_data(preprocessed_data)
+                    num_data.to_csv(csv_ready_file_num, index=True)
         else:
             print(f"File {ohlcv_file} is invalid.")
     else:
--- VN30F1M/transform/func_preprocessing.py
+++ VN30F1M/transform/func_preprocessing.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
 DROP_COLUMNS = ['time_d', 'couple_cs_1st_cond', 'couple_cs_2rd_cond']
 
 
@@ -5,3 +9,14 @@ def preprocessing_data(df):
     df.drop(columns=DROP_COLUMNS, inplace=True)
     df.dropna(inplace=True)
     return df
+
+
+def safe_int_convert(series):
+    le = LabelEncoder()
+    to_series = le.fit_transform(series)
+    return to_series
+
+def numeric_data(df):
+    df = df.apply(lambda col: safe_int_convert(col) if col.dtype == "object" else col)
+    return df
+    
